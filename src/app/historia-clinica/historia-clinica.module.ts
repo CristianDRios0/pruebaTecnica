0b0451cdabfec574e08501ed80b46e8323517e93
CristianDRios0 <cristiandrios@outlook.com>
2024-12-19 21:40:17 -0500

    Implementación de servicios HTTP

--- src/app/historia-clinica/historia-clinica.module.ts
+++ src/app/historia-clinica/historia-clinica.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { ListarHistoriaComponent } from './listar-historia/listar-historia.component';
 import { CrearHistoriaComponent } from './crear-historia/crear-historia.component';
 import { RouterModule, Routes, ROUTES } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 const routes: Routes = [
   {
@@ -22,7 +23,8 @@ const routes: Routes = [
   ],
   imports: [
     CommonModule,
-    RouterModule.forChild(routes)
+    RouterModule.forChild(routes),
+    FormsModule
   ]
 })
 export class HistoriaClinicaModule { }
